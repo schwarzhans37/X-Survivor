af3174b801aac2c34d8b634803e15a8f25ca4760
FriedenSehnsucht37 <169035573+schwarzhans37@users.noreply.github.com>
2025-10-10 14:01:41 +0900

    업그레이드 스크립트 파일 추가

---
+++ X_Suvivor_prototype/Assets/Scripts/DataManage/PetUpdateData.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PetUpgradeType
+{
+    AttackDamage,
+    AttackCooldown,
+    ProjectileCount,
+    MaxHealth
+}
+
+[CreateAssetMenu(fileName = "PetUpgrade_", menuName = "Pet/Pet Upgrade Data")]
+public class PetUpgradeData : ScriptableObject
+{
+    [Header("강화 타입")]
+    public PetUpgradeType upgradeType;
+
+    [Header("UI 정보")]
+    public string upgradeName;
+    [TextArea(3, 5)]
+    public string upgradeDescription;
+    public Sprite upgradeIcon;
+
+    [Header("강화 수치")]
+    public float value; // 각 타입에 맞는 수치를 여기에 입력 (예: 공격력 +5, 쿨감 -0.15f)
+}
